e5421e6f8599faf07051ff19d48c10c3743fcd50
Griff <MikeGriff81@gmail.com>
2025-02-27 08:44:12 -0500

    Update URL

--- app/app.py
+++ app/app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/resumes/mike')
+def resume_mike():
     return render_template('index.html')
