29bd54c3316b7ca47bbf99442f59ef57bdfc18c1
Griff <MikeGriff81@gmail.com>
2025-02-27 15:37:23 -0500

    ,

--- app/app.py
+++ app/app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def health_check():
+    return "Healthy", 200
+
 @app.route('/resumes/mike')
 def resume_mike():
     return render_template('index.html')
 
-@app.route('/')
-def health_check():
-    return "Healthy", 200
