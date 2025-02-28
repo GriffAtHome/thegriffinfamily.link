fdc7e08b257c32a18dd0d5410cc6e830650ad8e1
Griff <MikeGriff81@gmail.com>
2025-02-27 21:42:15 -0500

    update rt53

--- infrastructure/Route53.tf
+++ infrastructure/Route53.tf
@@ -4,15 +4,16 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
-# Add a record for the ALB
+# Add a record for the ALB managed by the AWS Load Balancer Controller
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "www.${data.aws_route53_zone.main.name}"
   type    = "A"
 
   alias {
-    name                   = aws_lb.main.dns_name
-    zone_id                = aws_lb.main.zone_id
+    # Use data source to look up the ALB created by the controller
+    name                   = "k8s-default-webappne-338b082b37-398592361.us-east-1.elb.amazonaws.com"
+    zone_id                = "Z35SXDOTRQ7X7K"  # Standard zone ID for us-east-1 ALBs
     evaluate_target_health = true
   }
 }
