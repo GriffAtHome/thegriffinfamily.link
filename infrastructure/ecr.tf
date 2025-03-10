91687c35271da89f7020d0b84c2644338e363ec5
Griff <MikeGriff81@gmail.com>
2025-03-10 06:56:35 -0400

    .

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = "${local.project_name}-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true  # Needed to delete the repository when it's not empty (used during TF destroy)
 
   image_scanning_configuration {
     scan_on_push = true
