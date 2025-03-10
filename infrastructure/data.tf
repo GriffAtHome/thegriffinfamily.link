879721a5689ae019571c8319ecdd624048f67a58
Griff <MikeGriff81@gmail.com>
2025-03-10 11:02:43 -0400

    .

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -10,16 +10,6 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
-locals {
-  # Define resource names
-  eks_cluster_name = "${local.project_name}-${local.environment}"
-  alb_name = "k8s-default-webappne-338b082b37"
-  
-  # Use try/can to safely check if resources exist
-  eks_cluster_exists = var.skip_data_sources ? false : can(data.aws_eks_cluster.cluster_check[0])
-  alb_exists = var.skip_data_sources ? false : can(data.aws_lb.ingress_alb_check[0])
-}
-
 # Check resources existence without failing
 data "aws_eks_cluster" "cluster_check" {
   count = var.skip_data_sources ? 0 : 1
